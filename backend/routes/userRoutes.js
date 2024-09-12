274711f67f4b5a2098c124be568cd7661c8157c5
Harsh3991 <dhobleharshwardhan@gmail.com>
2024-09-12 15:47:51 +0530

    Implemented the authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authorizeUser = require('../middleware/authorizeUser');
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
+router.get('/', authorizeUser, userController.getAllUsers);
+router.get('/:id', authorizeUser, userController.getUserById);
 router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.put('/:id', authorizeUser, userController.updateUser);
+router.delete('/:id', authorizeUser, userController.deleteUser);
 
 module.exports = router;
