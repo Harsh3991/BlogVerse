0d71fadf5532cdbbfdbff6bdaa3dd2155721acbd
Harsh3991 <dhobleharshwardhan@gmail.com>
2024-10-18 16:28:18 +0530

    creaed blogpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Profile from './components/Auth/Profile'
+import BlogList from './components/Blog/BlogList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Profile/>
+    <BlogList/>
     </>
   )
 }
