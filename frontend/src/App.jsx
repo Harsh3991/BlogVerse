15b81919ab10567e702998ff4f866a1b9ad25b07
Harsh3991 <dhobleharshwardhan@gmail.com>
2024-10-21 15:05:16 +0530

    created form page where user can write blog and paste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import BlogList from './components/Blog/BlogList'
+import CreateBlog from './components/Blog/CreateBlog';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <BlogList/>
+    <CreateBlog/>
     </>
   )
 }
